501d69bdb4f8e6bb0ac0ac4b50bc5175448967c7
NiveshhrajSivakumar <niveshhraj212004@gmail.com>
2023-12-02 14:11:52 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,7 +81,15 @@ document.querySelector('.js-products-grid').
             quantity:1
           });
         }
-        console.log(cart);
+
+        let cartQuantity=0;
+
+        cart.forEach((item)=>{
+          cartQuantity+=item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').
+          innerHTML=cartQuantity;
       });
     });
 
